137a3e54dcf074f0aba8482c02942a1797b87018
0xkrish03 <166608367+0xkrish03@users.noreply.github.com>
2024-10-10 00:13:28 +0530

    list animation happens in this file

---
+++ components/navbar/NavbarList.tsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { JSX } from "react";
+
+const links = [
+  {
+    href: "/",
+    name: "home",
+  },
+  {
+    href: "/",
+    name: "explore",
+  },
+  {
+    href: "/",
+    name: "about",
+  },
+  {
+    href: "/",
+    name: "menu",
+  },
+  {
+    href: "/",
+    name: "contact",
+  },
+];
+
+const letterAnimaton = {
+  initial: { y: "100%", opacity: 0 },
+  enter: (i: any[]) => ({
+    y: 0,
+    opacity: 1,
+    transition: { duration: 1.5, ease: [0.6, 0, 0.23, 1], delay: i[0] },
+  }),
+  exit: (i: any[]) => ({
+    y: "100%",
+    opacity: 0,
+    transition: { duration: 0.8, ease: [0.6, 0, 0.23, 1], delay: i[1] },
+  }),
+};
+
+const getLetter = (name: string) => {
+  let letters: JSX.Element[] = [];
+  name.split("").forEach((letter, index) => {
+    letters.push(
+      <motion.span
+        variants={letterAnimaton}
+        initial="initial"
+        animate="enter"
+        exit="exit"
+        custom={[index * 0.04, (name.length - index) * 0.01]}
+        key={index}
+      >
+        {letter}
+      </motion.span>
+    );
+  });
+  return letters;
+};
+
+const NavbarList = () => {
+  return (
+    <ul className="flex flex-col gap-8 font-primary text-4xl font-semibold text-accent items-center uppercase">
+      {links.map((link, index) => (
+        <li key={index}>
+          <Link
+            href={link.href}
+            className="flex overflow-hidden hover:text-white transition-all"
+          >
+            {getLetter(link.name)}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default NavbarList;
