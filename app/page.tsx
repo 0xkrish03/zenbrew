bb31a8ab4b3b171bb304c703bdfc2d3b454a343f
0xkrish03 <166608367+0xkrish03@users.noreply.github.com>
2024-10-13 11:43:21 +0530

    made changes to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,22 @@
 "use client";
 
+import Explore from "@/components/Explore";
 import Hero from "@/components/Hero";
 import React, { useEffect } from "react";
 
 const Home = () => {
   // add locomotive scroll
-  useEffect(()=>{
-    const loadLocomotiveScrol=async()=>{
-      const LocomotiveScroll=(await import("locomotive-scroll")).default;
-      new LocomotiveScroll()
-    }
-    loadLocomotiveScrol()
-  },[])
+  useEffect(() => {
+    const loadLocomotiveScrol = async () => {
+      const LocomotiveScroll = (await import("locomotive-scroll")).default;
+      new LocomotiveScroll();
+    };
+    loadLocomotiveScrol();
+  }, []);
   return (
     <div className="h-full overflow-x-hidden">
       <Hero />
+      <Explore />
       {/* temparory div */}
       <div className="h-[4000px]"></div>
     </div>
