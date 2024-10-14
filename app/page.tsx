bc981594fe7cb9d0132d59da4f65bdfb76162ea9
0xkrish03 <166608367+0xkrish03@users.noreply.github.com>
2024-10-14 23:12:06 +0530

    added a component

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@
 import About from "@/components/About";
 import Explore from "@/components/Explore";
 import Hero from "@/components/Hero";
+import Menu from "@/components/menu/Menu";
 import React, { useEffect } from "react";
 
 const Home = () => {
@@ -18,7 +19,10 @@ const Home = () => {
     <div className="h-full overflow-x-hidden">
       <Hero />
       <Explore />
-      <About />
+      <div>
+        <About />
+      </div>
+      <Menu />
       {/* temparory div */}
       <div className="h-[4000px]"></div>
     </div>
